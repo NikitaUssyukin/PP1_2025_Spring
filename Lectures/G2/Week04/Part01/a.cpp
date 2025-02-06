9c533a4cee064c6fd492ec03b9374691fafbe99e
NikitaUssyukin <nikus0108@gmail.com>
2025-02-06 12:46:35 +0500

    update

---
+++ Lectures/G2/Week04/Part01/a.cpp
@@ -0,0 +1,23 @@
+/*
+
+Control flow is the order in which the computer executes statements.
+
+In C++ it refers to the order in which individual statements, instructions, or function calls are executed within a program. 
+It determines how a program makes decisions and loops over code blocks based on conditions.
+
+Control structures help in altering the flow of execution.
+
+They are mainly categorized into three types:
+
+- Sequential control – default execution where statements run one after another in the order they appear
+
+- Selection (decision-making) statements – allow a program to make decisions based on conditions:
+    - if, else if, else
+    - switch-case
+
+- Iteration (looping) statements – used to execute a block of code multiple times:
+    - for loop
+    - while loop
+    - do-while loop
+
+*/
