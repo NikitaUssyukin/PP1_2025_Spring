df1142af0955c5b950bce5ac0caedd04cdf7b0a1
NikitaUssyukin <nikus0108@gmail.com>
2025-02-13 10:55:32 +0500

    update

---
+++ Lectures/G1/Week05/Lab03/310_1.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    int n; // number to check
+    cin >> n;
+
+    int n1, n2, n3, n4; // digits of the number
+
+    n1 = n / 1000;      // first digit
+
+    n2 = n / 100 % 10;  // second digit
+
+    n3 = n / 10 % 10;   // third digit
+
+    n4 = n % 10;        // fourth digit
+
+    cout << (n1 - n4) * (n1 - n4) + (n2 - n3) * (n2 - n3) + 1 << endl;
+
+    return 0;
+}
