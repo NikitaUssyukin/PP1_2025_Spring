2730413d79b36e9728224cc971e9a42f4dc4533e
NikitaUssyukin <nikus0108@gmail.com>
2025-02-27 12:50:23 +0500

    update

---
+++ Practices/G1/Week07/b.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cmath> // for M_PI
+
+using namespace std;
+
+int main() {
+
+    printf("%.4f", M_PI);
+
+    return 0;
+}
---
+++ Practices/G1/Week07/c.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cmath> // for M_PI
+
+using namespace std;
+
+int main() {
+
+    printf("%.2f %.4f %.10f\n", M_PI, M_PI, M_PI);
+
+    return 0;
+}
