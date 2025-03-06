00d505dd7a5a5bf7254ec3c4f48425ae1550ebc2
NikitaUssyukin <nikus0108@gmail.com>
2025-03-06 09:58:28 +0500

    update

---
+++ Lectures/G1/Week08/1d_arrays/b.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    int a[3]; // array with 3 elements
+    
+    a[0] = 2;
+    a[1] = 4;
+    a[2] = 6;
+
+    cout << a[0] << endl;
+    cout << a[1] << endl;
+    cout << a[2] << endl;
+
+    return 0;
+}
