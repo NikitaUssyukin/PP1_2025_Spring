fe5a7ca7120e8d02f5954bf6dd148a0b9c49122d
NikitaUssyukin <nikus0108@gmail.com>
2025-03-06 15:07:27 +0500

    update

--- Lectures/G2/Week08/1d_arrays/l.cpp
+++ Lectures/G2/Week08/1d_arrays/l.cpp
@@ -14,10 +14,10 @@ int main() {
         cin >> a[i];
     }
 
-    // reversing the array itself
-    // reverse(array_name + first_idx, array_name + last_idx + 1)
+    // sorting the array itself
+    // sort(array_name + first_idx, array_name + last_idx + 1)
     sort(a, a + n);
-    // reversed within the range [0; n)
+    // sorted within the range [0; n)
     // that is, the whole array
 
     for(int i = 0; i < n; ++i) { 
